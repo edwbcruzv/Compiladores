f3190699867416afcc64cfc01866929dac28ae68
cruz <edwin.cruz.v@outlook.com>
2021-04-13 21:22:29 -0500

    V6_Cruz

--- AFNe/AFNe.py
+++ AFNe/AFNe.py
@@ -1,26 +1,108 @@
-from PySimpleAutomata import DFA,automata_IO
-from Estado import *
-from Transicion import *
+
+class Estado:
+    #elem puede ser lista o string ya que esta sobrecargado
+    def __init__(self,elem,Token=0):
+        
+        if list==type(elem):
+            self.Nombre=elem[0]
+            self.Token=elem[1]
+        elif str==type(elem):
+            self.Nombre=elem
+            self.Token=Token
+        
+
+    def setNombre(self,Nombre):
+        self.Nombre=Nombre
+
+    #Al ser estado de aceptacion se requerira su token
+    def setToken(self,Token):
+        self.Token=Token
+
+    def getNombre(self):
+        return self.Nombre
+
+    def getToken(self):
+        return self.Token
+
+    def __lt__(self,estado):
+        return self.getNombre()<estado.getNombre()
+
+    def __le__(self,estado):
+        return self.getNombre()<=estado.getNombre()
+
+    def __eq__(self,estado):
+        return self.getNombre()==estado.getNombre()
+
+    def __str__(self):
+        #en el caso de ser un estado de aceptacion tambien se mostrara su token
+        if self.Token:
+            return "%s [shape=doublecircle]" %(
+                self.Nombre)
+        else: #como no es estado de aceptacion solo se mostrara su nombre
+            return self.Nombre
+
+
+class Transicion:
+    
+    def __init__(self,EdoPrincipal,NomEdoDestino,Simbolo):
+        
+        #Estado(Objeto) Principal
+        self.EstadoPrincipal=EdoPrincipal
+        #Estado(Objeto) Destino
+        self.EstadoDestino=NomEdoDestino
+        #Simbolo de transicion
+        self.Simbolo=Simbolo
+        
+
+    def setEstadoPrincipal(self,estado):
+        self.EstadoPrincipal=estado
+
+    def setEstadoDestino(self,estado):
+        self.EstadoDestino=estado
+
+    def setSimbolo(self,simbolo):
+        self.Simbolo=simbolo
+
+    def getEstadoPrincipal(self):
+        return self.EstadoPrincipal
+
+    def getEstadoDestino(self):
+        return self.EstadoDestino
+
+    def getSimbolo(self):
+        return self.Simbolo
+    
+    def __lt__(self,transicion):
+        return self.EstadoPrincipal<transicion.getEstadoPrincipal()
+
+    def __le__(self,transicion):
+        return self.EstadoPrincipal<=transicion.getEstadoPrincipal()
+
+    def __str__(self):
+
+        return "s%s -> s%s [label=\"%s\"] " %(
+            self.EstadoPrincipal.getNombre(),self.EstadoDestino.getNombre(),self.Simbolo.__str__())
 
 
 class AFN_e:
-    #Conjunto de Estados(Objeto) no vacios
-    K=[]  #K_list:Formato=[estado1,estado2,estado3,.....]
-
-    #Alfabeto que acepta el automata
-    Sigma=[] #Sigma_list:Formato=['a','b','c',.....]
-
-    #Estado de inicio del automata
-    S=object() #S:Formato= "nombre estado"
-
-    #Conjunto de Estados(Objeto) de aceptacion
-    Z=[] #Z_list:Formato= [estado1,estado2,estado3,.....]
-
-    #Conjunto de Transiciones(Objeto)
-    M=[] #M_list:Formato= [[sublista1],[sublista2],[sublista3],....]
-        #        sublista: Formato=[estado1,estado2,Simbolos]
+    
 
     def __init__(self,K_list,Sigma_list,S,Z_list,M_list):
+        #Conjunto de Estados(Objeto) no vacios
+        self.K=[]  #K_list:Formato=[estado1,estado2,estado3,.....]
+
+        #Alfabeto que acepta el automata
+        self.Sigma=[] #Sigma_list:Formato=['a','b','c',.....]
+
+        #Estado de inicio del automata
+        self.S=object() #S:Formato= "nombre estado"
+
+        #Conjunto de Estados(Objeto) de aceptacion
+        self.Z=[] #Z_list:Formato= [estado1,estado2,estado3,.....]
+
+        #Conjunto de Transiciones(Objeto)
+        self.M=[] #M_list:Formato= [[sublista1],[sublista2],[sublista3],....]
+            #        sublista: Formato=[estado1,estado2,Simbolos]
         #primero se empieza a definir y a crear los estados de aceptacion, ya que
         #para los demas atributos necesitaremos la lista de todos los estados
         self.__ListEstadosObjs(K_list)
@@ -36,10 +118,6 @@ class AFN_e:
 
         #se definira las transiciones
         self.__ListTransicionesObjs(M_list)
-
-    def ordenaEdos(self):
-        pass
-
 
 #------------------------OPERACIONES DE LOS AUTOMATAS-------------------
     #La funcion regresara un nuevo estado inicial en base a los estados ya existente
@@ -57,6 +135,21 @@ class AFN_e:
     def unirCon(self,automata2):
         #se crea el nuevo estado inicial
         nuevo_edo_inicial=self.nuevoEdo()
+        
+        #antes de hacer la union se debe de renombrar todos los estados edel automata2
+        self_tam=len(self.K) #cantidad de estados que tiene este automata
+        a2_tam=len(automata2.K) #cantidad de estadoa del automata2
+        
+        for i in  range(a2_tam):
+            automata2.K[i].setNombre(str(i+self_tam)) #se continuara la numeracion de cada estado
+
+        #Los estados de automata2 se unen con la lista de estados de este automata
+        self.K.extend(automata2.K)
+        #ahora las transiciones de automata2 de uniran con este automata
+        self.M.extend(automata2.M)
+        #ahora se unen los afabetos
+        self.Sigma.extend(automata2.Sigma)
+
         #se crea el nuevo estado final
         nuevo_edo_final=self.nuevoEdo()
 
@@ -65,39 +158,43 @@ class AFN_e:
         tam1=len(self.getEstadosAceptacion())
         for i in range(tam1):
             edo_final1=self.getEstadosAceptacion()[i]
-            self.__ListTransicionesObjs([[edo_final1,nuevo_edo_final,"E"]])
+            self.__ListTransicionesObjs([[edo_final1.getNombre(),nuevo_edo_final.getNombre(),"E"]])
 
         #si el 'automata2' tiene mas estados finales
         #todos estos se unical al nuevo estado final
         tam2=len(automata2.getEstadosAceptacion())
         for i in range(tam2):
             edo_final2=automata2.getEstadosAceptacion()[i]
-            self.__ListTransicionesObjs([[edo_final2,nuevo_edo_final,"E"]])
+            self.__ListTransicionesObjs([[edo_final2.getNombre(),nuevo_edo_final.getNombre(),"E"]])
         
         #ahora se unira el nuevo estado inicial
         #con el estado inicial de este automata
         edo_init1=self.getEdoInicial()
-        self.__ListTransicionesObjs([[nuevo_edo_inicial,edo_init1,"E"]])
+        self.__ListTransicionesObjs([[nuevo_edo_inicial.getNombre(),edo_init1.getNombre(),"E"]])
         
         #ahora se unira el nuevo estado inicial
         #con el estado inicial de 'automata2'
         edo_init2=automata2.getEdoInicial()
-        self.__ListTransicionesObjs([[nuevo_edo_inicial,edo_init2,"E"]])
+        self.__ListTransicionesObjs([[nuevo_edo_inicial.getNombre(),edo_init2.getNombre(),"E"]])
 
         self.S=nuevo_edo_inicial #redefiniendo los nuevos estados iniciales creados
         self.Z=[nuevo_edo_final] #redefiniendo los nuevos estados finales creados
 
     def concatCon(self,automata2):
 
-        #se crea el nuevo estado inicial
-        nuevo_edo_inicial=self.nuevoEdo()
-        #se crea el nuevo estado final
-        nuevo_edo_final=self.nuevoEdo()
+        #antes de hacer la concatenacion  se debe de renombrar todos los estados edel automata2
+        self_tam=len(self.K) #cantidad de estados que tiene este automata
+        a2_tam=len(automata2.K) #cantidad de estadoa del automata2
+        
+        for i in  range(a2_tam):
+            automata2.K[i].setNombre(str(i+self_tam)) #se continuara la numeracion de cada estado
 
-        #ahora se unira el nuevo estado inicial
-        #con el estado inicial de este automata
-        edo_init1=self.getEdoInicial()
-        self.__ListTransicionesObjs([[nuevo_edo_inicial,edo_init1,"E"]])
+        #Los estados de automata2 se unen con la lista de estados de este automata
+        self.K.extend(automata2.K)
+        #ahora las transiciones de automata2 de uniran con este automata
+        self.M.extend(automata2.M)
+        #ahora se unen los afabetos
+        self.Sigma.extend(automata2.Sigma)
 
         #si este automata tiene mas estados finales
         #todos estos se uniran al estado inicial de
@@ -105,16 +202,19 @@ class AFN_e:
         tam1=len(self.getEstadosAceptacion())
         for i in range(tam1):
             edo_final=self.getEstadosAceptacion()[i]
-            self.__ListTransicionesObjs([[edo_final,automata2.getEdoInicial(),"E"]])
+            self.__ListTransicionesObjs([[edo_final.getNombre(),automata2.getEdoInicial().getNombre(),"E"]])
+
+        #se crea el nuevo estado final
+        nuevo_edo_final=self.nuevoEdo()
 
         #si el 'automata2' tiene mas estados finales
         #todos estos se inicial al nuevo estado final
         tam2=len(automata2.getEstadosAceptacion())
         for i in range(tam2):
             edo_final2=automata2.getEstadosAceptacion()[i]
-            self.__ListTransicionesObjs([[edo_final2,nuevo_edo_final,"E"]])
+            self.__ListTransicionesObjs([[edo_final2.getNombre(),nuevo_edo_final.getNombre(),"E"]])
 
-        self.S=nuevo_edo_inicial #redefiniendo los nuevos estados iniciales creados
+        # self.S=nuevo_edo_inicial #redefiniendo los nuevos estados iniciales creados
         self.Z=[nuevo_edo_final] #redefiniendo los nuevos estados finales creados
 
     def cerraduraPositiva(self):
@@ -125,9 +225,9 @@ class AFN_e:
 
         #si este automata tiene mas estados finales
         #todos estos se uniran al nuevo estado final
-        tam1=len(self.getEstadosAceptacion())
-        for i in range(tam1):
-            edo_final1=self.getEstadosAceptacion()[i]
+        tam=len(self.getEstadosAceptacion())
+        for i in range(tam):
+            edo_final1=self.Z[i]
             t=[edo_final1.getNombre(),nuevo_edo_final.getNombre(),"E"]
             self.__ListTransicionesObjs([t])
         
@@ -166,10 +266,10 @@ class AFN_e:
 
         #si este automata tiene mas estados finales
         #todos estos se uniran al nuevo estado final
-        tam1=len(self.getEstadosAceptacion())
-        for i in range(tam1):
-            edo_final1=self.getEstadosAceptacion()[i]
-            t=[edo_final1.getNombre(),nuevo_edo_final.getNombre(),"E"]
+        tam=len(self.getEstadosAceptacion())
+        for i in range(tam):
+            edo_final=self.Z[i]
+            t=[edo_final.getNombre(),nuevo_edo_final.getNombre(),"E"]
             self.__ListTransicionesObjs([t])
 
         #se crea la transicion del estado inicial al final
@@ -264,27 +364,16 @@ class AFN_e:
             
         
     def mostrarAutomata(self):
-        
-        f = open("afn.dot", "w+")
-        f.write("digraph{\nfake [style=invisible]\nfake -> s0 [style=bold]\n\n")
-
 
         print("Alfabeto que acepta el automata")
         print(self.Sigma)
 
         print("Estado de inicio del automata")
         print(self.S.__str__())
-        f.write("s" + self.S.__str__() + " [root = true]\n")
-        f.write("\n")
 
         print("Conjunto de Estados(Objeto) no vacios")
         for e in self.K:
            print(e.__str__())
-
-        for e in self.K[1:]:
-           f.write('s'+str(e.__str__())+'\n')
-
-        f.write("\n")
 
         print("Conjunto de Estados(Objeto) de aceptacion")
         for e in self.Z:
@@ -293,10 +382,3 @@ class AFN_e:
         print("Conjunto de Transiciones(Objeto)")
         for e in self.M:
             print(e.__str__())
-            f.write(e.__str__()+"\n")
-
-        f.write("\n}")
-        f.close()
-
-        afn = automata_IO.nfa_dot_importer("afn.dot")
-        automata_IO.nfa_to_dot(afn, "conv", ".")
--- main.py
+++ main.py
@@ -1,47 +1,50 @@
-from AFN_e import *
+from AFNe.AFNe import AFN_e
 
+def automata1():
+    K_list1=['0','1']
+    Sigma_list1=['h']
+    S1='0'
+    Z_list1=[['1',41]]
+    T1=['0','1','h']
+    M_list1=[T1]
+    A1=AFN_e(K_list1,Sigma_list1,S1,Z_list1,M_list1)
+    return A1
+
+def automata2():
+    K_list1=['0','1']
+    Sigma_list1=['i']
+    S1='0'
+    Z_list1=[['1',42]]
+    T1=['0','1','i']
+    M_list1=[T1]
+    A1=AFN_e(K_list1,Sigma_list1,S1,Z_list1,M_list1)
+    return A1
+
+def automata3():
+    K_list1=['0','1']
+    Sigma_list1=['j']
+    S1='0'
+    Z_list1=[['1',43]]
+    T1=['0','1','j']
+    M_list1=[T1]
+    A1=AFN_e(K_list1,Sigma_list1,S1,Z_list1,M_list1)
+    return A1
 
 def main():
-    
-    #los estados unicamente seran numeros enteros
-    #se tendra formato 's0','s1','s2','S3',......
-    #pero este rubro se llena solo al construir el automata
-    
-    #estados=["0","1","2","3","4","5","6","7"]
-    #alfabeto=['a','b','c']
-    #el estado inicial solo sera un numero entero
-    #estado_inicial='0'
-    #los estados de aceptacion tambn seran 's10','s11','s12','S13',......
-    #y los toquen seran entero sin formato de cadena 10,25,416,78,14,.........
-    #estados_aceptacion=[["7",47],["10",45]]
-    #Las transiciones se agregarar como sub listas
-    #t1=["0","1","E"]
-    # t10=["0","7","E"]
-    # t2=["1","2","E"]
-    # t3=["1","4","E"]
-    # t4=["2","3","a"]
-    # t5=["4","5","b"]
-    # t6=["3","6","E"]
-    # t7=["5","6","E"]
-    # t8=["6","1","E"]
-    # t9=["6","7","E"]
-    # transiciones=[t1,t2,t3,t4,t5,t6,t7,t8,t9,t10]
+    print("------------------------------------automata1")
+    A1=automata1()
+    A1.mostrarAutomata()
+    print("------------------------------------automata2")
+    A2=automata2()
+    A2.mostrarAutomata()
+    print("------------------------------------automata3")
+    A3=automata3()
+    A3.mostrarAutomata()
 
-    estados=['0','1']
-    alfabeto=['a']
-    estado_inicial='0'
-    estados_aceptacion=[["1",47]]
-    t1=["0","1","a"]
-    transiciones=[t1]
-    a=AFN_e(estados,alfabeto,estado_inicial,estados_aceptacion,transiciones)
-    a.cerraduraKleene()
-    #a.cerraduraPositiva()
-    #a.opcion()
-    #a.ordenaEdos()
-    a.mostrarAutomata()
-
-
-
+    print("------------------------------------concat")
+    A1.unirCon(A2)
+    A1.unirCon(A3)
+    A1.mostrarAutomata()
 
 
 
